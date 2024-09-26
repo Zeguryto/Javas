3a811131d0b58eb969a470b8f06a8acb2a2ba31d
Zeguryto <evelina.holm@edu.prakticum.fi>
2024-09-26 13:29:48 +0300

    My first commit

---
+++ Javapersonlig.java
@@ -0,0 +1,22 @@
+
+public class Javapersonlig {
+
+
+    public static void main(String args[]) {
+        String name = "Anton";
+        String lastName = "Sällström";
+        System.out.println(name +" " + lastName);
+        
+        int myNum = 32;
+        System.out.println (myNum);
+        
+        double height = 1.72;
+        System.out.println (height);
+        
+        boolean student = false;
+        
+        System.out.println("false");
+    
+       
+    }
+}

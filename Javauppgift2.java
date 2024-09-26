c202a9bd24aebff59cfeaec272954776e5cb22c6
Zeguryto <evelina.holm@edu.prakticum.fi>
2024-09-26 13:33:35 +0300

    Add files via upload

---
+++ Javauppgift2.java
@@ -0,0 +1,18 @@
+
+public class Javauppgift2 {
+
+
+    public static void main(String args[]) {
+
+String txt = "I hate to code Java";
+        System.out.println("The length of the text is:"+txt.length());
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+        System.out.println(txt.indexOf("J"));
+        
+        int x = 15;
+        int y = 35;
+        int z = x + y;
+        System.out.println(z);
+    }
+}
